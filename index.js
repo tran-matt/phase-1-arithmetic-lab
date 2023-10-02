3ef30bbd28d44003c9af3404df14d15f00cd5f4b
Matthew Tran <matt.h.tran@gmail.com>
2023-10-02 16:04:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 16;
+const num4 = 4;
+const mod = num3 / num4;
+
+const max = Math.max(1,5,10,20);
